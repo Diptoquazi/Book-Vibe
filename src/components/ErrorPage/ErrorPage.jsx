c26ea03f18bd3fc49a38840879b0c8fe7f47a379
Quazi A.Q.M. Ziaus Shams <155340045+Diptoquazi@users.noreply.github.com>
2024-05-21 22:53:49 +0600

    added errorPage

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
     return (
-        <div>
-            
+        <div className="text-center mt-80">
+            <h2 className="text-6xl font-extrabold mb-16">Oppss!!!</h2>
+            <Link className="bg-[#23BE0A] text-white font-bold text-4xl rounded-lg px-10 py-3" to="/">Go Back</Link>
         </div>
     );
 };
